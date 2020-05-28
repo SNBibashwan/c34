dee5158ffa791cb3570928e8a51a529f235ba765
SNBibashwan <63007082+SNBibashwan@users.noreply.github.com>
2020-05-28 23:36:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+
+var paint = [];
+var nline = [];
+function setup() {
+  canvas = createCanvas(800,400);
+  canvas.mousePressed(sline);
+
+}
+
+function draw() {
+  background("lightskyblue");
+  stroke("black");
+  strokeWeight(1);
+  text("Press the SpaceBar to clear the canvas!",40,15);
+
+  if(mouseIsPressed){
+    var draw = {
+      x: mouseX,
+      y: mouseY
+    }
+    nline.push(draw);
+  }
+
+  var color = "white";
+  if(keyCode === UP_ARROW){
+    color = "lightskyblue";
+  }
+
+  stroke(color);
+  strokeWeight(10);
+  noFill();
+  for(var i = 0; i < paint.length; i++){
+    var ndraw = paint[i];
+    beginShape();
+    for(var m = 0; m < ndraw.length; m++){
+      vertex(ndraw[m].x,ndraw[m].y);
+    }
+    endShape();
+  }
+}
+
+function sline(){
+  nline = [];
+  paint.push(nline);
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+    paint = [];
+  }
+
+}
